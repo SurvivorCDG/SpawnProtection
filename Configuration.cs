317f76889b4f46bf37a23c6b675701f432786808
SurvivorCDG <survivor@xaker.ru>
2016-02-24 22:44:47 +0400

    Added files via upload

---
+++ Configuration.cs
@@ -0,0 +1,18 @@
+﻿using Rocket.API;
+
+namespace SpawnProtection
+{
+    public class Configuration : IRocketPluginConfiguration
+    {
+        public bool Enabled;
+        public int ProtectionTime;
+        public string ColorOfMessage;
+
+        public void LoadDefaults()
+        {
+            Enabled = true;
+            ProtectionTime = 10;
+            ColorOfMessage = "Blue";
+        }
+    }
+}
